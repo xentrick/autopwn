3fa666eabdc8a9738c8ffdc100cbc42448af9cd5
gserrao <gserrao@sourcefire.com>
2019-07-24 15:05:55 -0400

    Package for pip

--- autopwn/__init__.py
+++ autopwn/__init__.py
@@ -1,26 +1,5 @@
-#!/usr/bin/env python3
-
-import sys
-import logging
-
 from . import services
 from . import util
+from . import const
+from . import exceptions
 from .autopwn import Autopwn
-
-logging.basicConfig(level=logging.INFO)
-log = logging.getLogger("autopwn.__init__")
-log.addHandler(logging.NullHandler())
-
-
-def _real_main():
-    log.info("Initializing")
-    giggity = Autopwn()
-    giggity.scoreHost("127.0.0.1")
-    sys.exit(0)
-
-
-def main():
-    _real_main()
-
-
-__all__ = [n for n in globals().keys() if n[:1] != "_"]
--- autopwn/util/__init__.py
+++ autopwn/util/__init__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+
 
 from . import ip
 from . import helper
